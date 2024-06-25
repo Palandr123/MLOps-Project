8f59096f6a3a710cf4e400568df12b3064e178db
Vladimir Bazilevich <v.bazilevich@innopolis.university>
2024-06-25 15:14:40 +0300

    Added data validation

--- src/data.py
+++ src/data.py
@@ -1,10 +1,13 @@
 import os
+import sys
 from subprocess import run
 from pathlib import Path
 from zipfile import ZipFile
 
 from hydra import compose, initialize
 import pandas as pd
+import great_expectations as gx
+from great_expectations.data_context import FileDataContext
 
 
 def download_data(user_name: str, dataset_name: str, save_path: str | Path):
@@ -79,6 +82,169 @@ def sample_data() -> pd.DataFrame:
     # Return sampled data
     return sample
 
+def validate_initial_data() -> bool:
+    """
+    Validates a data sample against specified expectations
+    
+    Returns:
+        bool: validation result
+    """
+    
+    # Set up data sample access
+    context = FileDataContext(project_root_dir='services')
+    sample_source = context.sources.add_or_update_pandas('data_sample')
+    sample_asset = sample_source.add_csv_asset(name='data_sample_asset', filepath_or_buffer='data/samples/sample.csv')
+    batch_request = sample_asset.build_batch_request()
+    batches = sample_asset.get_batch_list_from_batch_request(batch_request)
+    
+    # Create expectations suite
+    context.add_or_update_expectation_suite('expectation_suite')
+    
+    # Create validator
+    validator = context.get_validator(
+        batch_request=batch_request,
+        expectation_suite_name='expectation_suite',
+    )
+    
+    # Define expectations
+    # Ex 1
+    validator.expect_column_values_to_not_be_null(
+        column='fuel',
+        mostly=0.75,
+    )
+    # Ex 2
+    validator.expect_column_values_to_be_in_set(
+        column='fuel',
+        value_set=['gas', 'diesel', 'hybrid', 'electric', 'other']
+    )
+    # Ex 3
+    validator.expect_column_most_common_value_to_be_in_set(
+        column='fuel',
+        value_set=['gas'],
+        ties_okay=True,
+    )
+    # Ex 4
+    validator.expect_column_values_to_not_be_null(
+        column='manufacturer',
+        mostly=0.75,
+    )
+    # Ex 5
+    validator.expect_column_unique_value_count_to_be_between(
+        column='manufacturer',
+        min_value=1,
+        max_value=100,
+    )
+    # Ex 6
+    validator.expect_column_value_lengths_to_be_between(
+        column='manufacturer',
+        min_value=1,
+        max_value=60,
+    )
+    # Ex 7
+    validator.expect_column_values_to_not_be_null(
+        column='VIN',
+        mostly=0.5,
+    )
+    # Ex 8
+    validator.expect_column_values_to_match_regex(
+        column='VIN',
+        regex='^(?=.*[0-9])(?=.*[A-z])[0-9A-z-]{17}$',
+        mostly=0.5,
+    )
+    # Ex 9
+    validator.expect_column_values_to_be_null(
+        column='county'
+    )
+    # Ex 10
+    validator.expect_column_values_to_not_be_null(
+        column='transmission',
+        mostly=0.7,
+    )
+    # Ex 11
+    validator.expect_column_values_to_be_in_set(
+        column='transmission',
+        value_set=['automatic', 'manual'],
+        mostly=0.7,
+    )
+    # Ex 12
+    validator.expect_column_most_common_value_to_be_in_set(
+        column='transmission',
+        value_set=['automatic'],
+        ties_okay=True,
+    )
+    # Ex 13
+    validator.expect_column_values_to_not_be_null(
+        column='type',
+        mostly=0.75,
+    )
+    # Ex 14
+    validator.expect_column_values_to_be_in_set(
+        column='type',
+        value_set=['sedan', 'SUV', 'pickup',
+                   'truck', 'coupe', 'hatchback',
+                   'min-van', 'offroad', 'bus', 'van',
+                   'counvertible', 'wagon', 'other'],
+        mostly=0.75,
+    )
+    # Ex 15
+    validator.expect_column_unique_value_count_to_be_between(
+        column='type',
+        min_value=7,
+    )
+    # Ex 16
+    validator.expect_column_values_to_not_be_null(
+        column='lat',
+        mostly=0.9,
+    )
+    # Ex 17
+    validator.expect_column_values_to_be_between(
+        column='lat',
+        min_value=-90,
+        max_value=90,
+    )
+    # Ex 18
+    validator.expect_column_values_to_not_be_null(
+        column='long',
+        mostly=0.9,
+    )
+    # Ex 19
+    validator.expect_column_values_to_be_between(
+        column='long',
+        min_value=-180,
+        max_value=180,
+    )
+    # Ex 20
+    validator.expect_column_values_to_not_be_null(
+        column='drive',
+        mostly=0.6,
+    )
+    # Ex 21
+    validator.expect_column_values_to_be_in_set(
+        column='drive',
+        value_set=['fwd', 'rwd', '4wd'],
+        mostly=0.6,
+    )
+    
+    # Store expectation suite
+    validator.save_expectation_suite(
+        discard_failed_expectations = False
+    )
+    
+    # Create checkpoint
+    checkpoint = context.add_or_update_checkpoint(
+        name="checkpoint",
+        validator=validator,
+    )
+    
+    # Run validation
+    checkpoint_result = checkpoint.run()
+    return checkpoint_result.success
+
 
 if __name__ == "__main__":
     sample_data()
+    if not validate_initial_data():
+        print('Data sample fails to pass data validation')
+        sys.exit(1) # return non-zero code to simplify interactions with shell scripts
+    else:
+        print('Data sample validated successfully')
