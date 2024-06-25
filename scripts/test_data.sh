118047bef681b96f8b662a47b4458a0b2e602603
Vladimir Bazilevich <v.bazilevich@innopolis.university>
2024-06-25 16:18:33 +0300

    Initial version of test_data.sh

---
+++ scripts/test_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Backup config file
+cp configs/sample_data.yaml configs/sample_data.yaml.bkp
+
+# Validate samples
+for i in $(seq 1 5)
+do
+    echo "Testing batch #$i"
+    # Modify config
+    sed -i "s/sample_num.*/sample_num: $i/" configs/sample_data.yaml
+    python src/data.py
+done
+
+# Cleanup
+mv configs/sample_data.yaml.bkp configs/sample_data.yaml
