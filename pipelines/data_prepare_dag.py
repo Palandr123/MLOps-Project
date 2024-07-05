2abf7d01739481ce15c06cc81626c91db34f1ac5
Dinislam36 <dingab36@gmail.com>
2024-07-05 17:07:45 +0300

    added dag for data preparation

---
+++ pipelines/data_prepare_dag.py
@@ -0,0 +1,32 @@
+from pendulum import datetime
+from datetime import timedelta
+import os
+
+from airflow import DAG, AirflowException
+from airflow.decorators import task, dag
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.sensors.external_task import ExternalTaskSensor
+
+
+with DAG(dag_id="prepare_data",
+         start_date=datetime(2024, 6, 29, tz="UTC"),
+         schedule="*/30 * * * *",
+         catchup=False) as dag:
+    
+    sensor = ExternalTaskSensor(
+        task_id='wait_for_data_extract',
+        external_dag_id='extract_data',
+        execution_delta=timedelta(hours=1),
+        dag=dag,
+    )
+
+    data_prepare_command = "python ./pipelines/data_prepare.py "
+    data_prepare = BashOperator(
+        task_id= 'version_data',
+        # TODO: use config
+        bash_command=f"cd $PROJECT_HOME && {data_prepare_command}",
+        dag=dag
+    )
+
+    sensor >> data_prepare
