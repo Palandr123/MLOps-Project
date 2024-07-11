a4d6e93ff2ae0f2a0cf8130c7b885967cdadc351
Palandr123 <palandr545@gmail.com>
2024-07-11 20:12:04 +0000

    ! fix script

--- scripts/test_data.sh
+++ scripts/test_data.sh
@@ -3,28 +3,26 @@
 # Backup config file
 cp configs/sample_data.yaml configs/sample_data.yaml.bkp
 
-# Validate samples
-for i in $(seq 1 5)
-do
-    echo "Testing batch #$i"
-    # Modify config
-    sed -i "s/sample_num.*/sample_num: $i/" configs/sample_data.yaml
-    python src/data.py
+# Get sample_num from config
+sample_num=$(yq .sample_num configs/sample_data.yaml)
 
-    if [ $? -eq 0 ]
-    then
-        echo "Validation succeeded. Saving a new version of data"
-        dvc add data/samples/sample.csv
-        git add data/samples/sample.csv.dvc
-        git commit -m "Added batch $i"
-        git push
-        git tag -f -a "v1.0.$i" -m "Added batch $i"
-        git push -f --tags
-        dvc push
-    else
-        echo "Validation failed"
-    fi
-done
+# Validate samples
+echo "Testing batch #$sample_num"
+python src/data.py
+
+if [ $? -eq 0 ]
+then
+    echo "Validation succeeded. Saving a new version of data"
+    dvc add data/samples/sample.csv
+    git add data/samples/sample.csv.dvc
+    git commit -m "Added batch $sample_num"
+    git push
+    git tag -f -a "v1.0.$sample_num" -m "Added batch $sample_num"
+    git push -f --tags
+    dvc push
+else
+    echo "Validation failed"
+fi
 
 # Cleanup
 mv configs/sample_data.yaml.bkp configs/sample_data.yaml
--- services/airflow/dags/data_prepare.py
+++ services/airflow/dags/data_prepare.py
@@ -5,65 +5,63 @@ from src.data import read_datastore, preprocess_data, validate_features, load_fe
 
 
 @step(enable_cache=False)
-def extract()-> Tuple[
-                Annotated[pd.DataFrame,
-                        ArtifactConfig(name="extracted_data", 
-                                       tags=["data_preparation"]
-                                       )
-                        ],
-                Annotated[str,
-                        ArtifactConfig(name="data_version",
-                                       tags=["data_preparation"])]
-                    ]:
-    
+def extract() -> Tuple[
+    Annotated[
+        pd.DataFrame, ArtifactConfig(name="extracted_data", tags=["data_preparation"])
+    ],
+    Annotated[str, ArtifactConfig(name="data_version", tags=["data_preparation"])],
+]:
+
     df, version = read_datastore()
 
     return df, str(version)
 
+
 @step(enable_cache=False)
-def transform(df: pd.DataFrame)-> Tuple[
-                    Annotated[pd.DataFrame, 
-                            ArtifactConfig(name="input_features",
-                                           tags=["data_preparation"])],
-                    Annotated[pd.DataFrame,
-                            ArtifactConfig(name="input_target", 
-                                            tags=["data_preparation"])]
-                                    ]:
+def transform(
+    df: pd.DataFrame,
+) -> Tuple[
+    Annotated[
+        pd.DataFrame, ArtifactConfig(name="input_features", tags=["data_preparation"])
+    ],
+    Annotated[
+        pd.DataFrame, ArtifactConfig(name="input_target", tags=["data_preparation"])
+    ],
+]:
 
     # Your data transformation code
     X, y = preprocess_data(df)
 
     return X, y
 
+
 @step(enable_cache=False)
-def validate(X:pd.DataFrame, 
-             y:pd.DataFrame)->Tuple[
-                    Annotated[pd.DataFrame, 
-                            ArtifactConfig(name="valid_input_features",
-                                           tags=["data_preparation"])],
-                    Annotated[pd.DataFrame,
-                            ArtifactConfig(name="valid_target",
-                                           tags=["data_preparation"])]
-                                    ]:
+def validate(
+    X: pd.DataFrame, y: pd.DataFrame
+) -> Tuple[
+    Annotated[
+        pd.DataFrame,
+        ArtifactConfig(name="valid_input_features", tags=["data_preparation"]),
+    ],
+    Annotated[
+        pd.DataFrame, ArtifactConfig(name="valid_target", tags=["data_preparation"])
+    ],
+]:
 
     X, y = validate_features(X, y)
-    
+
     return X, y
 
 
 @step(enable_cache=False)
-def load(X:pd.DataFrame, y:pd.DataFrame, version: str)-> Tuple[
-                    Annotated[pd.DataFrame, 
-                            ArtifactConfig(name="features",
-                                           tags=["data_preparation"])],
-                    Annotated[pd.DataFrame,
-                            ArtifactConfig(name="target",
-                                           tags=["data_preparation"])]
-                                    ]:
-    
-    load_features(X, y, version)
+def load(
+    X: pd.DataFrame, y: pd.DataFrame, version: str
+) -> Annotated[
+    pd.DataFrame, ArtifactConfig(name="features_target", tags=["data_preparation"])
+]:
 
-    return X, y
+    return pd.concat([X, y], axis=1)
+
 
 @pipeline()
 def data_prepare_pipeline():
