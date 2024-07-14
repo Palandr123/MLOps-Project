0bfd9453ed11b80bb81b29ab4029088e940be6f0
Palandr123 <palandr545@gmail.com>
2024-07-14 21:33:23 +0000

    ! fix model issues

--- src/models.py
+++ src/models.py
@@ -1,12 +1,19 @@
+import random
+import numpy as np
+
+import torch
 from torch import nn
 
 
 class SimpleNN(nn.Module):
-    def __init__(self, input_size, hidden_units, output_size):
+    def __init__(self, input_size, hidden_units, output_size, seed):
+        random.seed(int(seed))
+        np.random.seed(int(seed))
+        torch.manual_seed(int(seed))
         super(SimpleNN, self).__init__()
-        self.hidden = nn.Linear(input_size, hidden_units)
+        self.hidden = nn.Linear(int(input_size), int(hidden_units))
         self.relu = nn.ReLU()
-        self.output = nn.Linear(hidden_units, output_size)
+        self.output = nn.Linear(int(hidden_units), int(output_size))
 
     def forward(self, x):
         x = self.hidden(x)
