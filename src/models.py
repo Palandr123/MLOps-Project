86aa11e22caca691cbb92aa92fbab9b9bd25ab57
Palandr123 <palandr545@gmail.com>
2024-07-17 21:53:43 +0000

    = make MLP of 3 layers

--- src/models.py
+++ src/models.py
@@ -6,17 +6,23 @@ from torch import nn
 
 
 class SimpleNN(nn.Module):
-    def __init__(self, input_size, hidden_units, output_size, seed):
+    def __init__(self, input_size, hidden_units1, hidden_units2, hidden_units3, output_size, seed):
         random.seed(int(seed))
         np.random.seed(int(seed))
         torch.manual_seed(int(seed))
         super(SimpleNN, self).__init__()
-        self.hidden = nn.Linear(int(input_size), int(hidden_units))
+        self.hidden1 = nn.Linear(int(input_size), int(hidden_units1))
+        self.hidden2 = nn.Linear(int(hidden_units1), int(hidden_units2))
+        self.hidden3 = nn.Linear(int(hidden_units2), int(hidden_units3))
         self.relu = nn.ReLU()
-        self.output = nn.Linear(int(hidden_units), int(output_size))
+        self.output = nn.Linear(int(hidden_units3), int(output_size))
 
     def forward(self, x):
-        x = self.hidden(x)
+        x = self.hidden1(x)
+        x = self.relu(x)
+        x = self.hidden2(x)
+        x = self.relu(x)
+        x = self.hidden3(x)
         x = self.relu(x)
         x = self.output(x)
         return x
